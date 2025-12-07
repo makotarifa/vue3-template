10c299995f6ab7b32dec1bf5cccdc1cfbf7f381e
makotarifa <angeljesusmorgallego@gmail.com>
2025-12-08 00:52:39 +0100

    chore(build): exclude mocks directory from production builds via vite plugin

--- vite.config.js
+++ vite.config.js
@@ -6,52 +6,74 @@ import { PrimeVueResolver } from 'unplugin-vue-components/resolvers'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      imports: [
-        'vue',
-        'vue-router',
-        'pinia',
-        {
-          'vee-validate': [
-            'useField',
-            'useForm',
-            'useFieldArray',
-            'handleSubmit',
-            'defineRule',
-            'configure',
-          ],
-        },
-        {
-          '@vueuse/core': [
-            // you can add functions to auto import if desired
-          ],
-        },
-      ],
-      dts: 'src/auto-imports.d.ts',
-      dirs: ['src/composables'],
-      vueTemplate: true,
-    }),
-    Components({
-      resolvers: [
-        PrimeVueResolver({
-          importStyle: false,
-          importIcons: true,
-          importTheme: false,
-        }),
-      ],
-      dirs: [
-        'src/components',
-        'src/domain/**/components',
-      ],
-      dts: 'src/components.d.ts',
-    }),
-  ],
-  resolve: {
+export default defineConfig(({ command, mode }) => {
+  const isProd = mode === 'production';
+
+  // Custom plugin to stub out `mocks` imports in production builds
+  const excludeMocksPlugin = {
+    name: 'exclude-mocks-prod',
+    enforce: 'pre',
+    load(id) {
+      if (!isProd) return null;
+      // Match imports that reference a `mocks` folder anywhere in the id
+      const mocksRegexp = /[\\/]mocks[\\/]/;
+      if (mocksRegexp.test(id)) {
+        // Return an empty module so the mocks aren't bundled into production
+        return 'export const __is_mock_placeholder = true; export default {}';
+      }
+      return null;
+    },
+  };
+
+  return {
+    plugins: [
+      vue(),
+      AutoImport({
+        imports: [
+          'vue',
+          'vue-router',
+          'pinia',
+          {
+            'vee-validate': [
+              'useField',
+              'useForm',
+              'useFieldArray',
+              'handleSubmit',
+              'defineRule',
+              'configure',
+            ],
+          },
+          {
+            '@vueuse/core': [
+              // you can add functions to auto import if desired
+            ],
+          },
+        ],
+        dts: 'src/auto-imports.d.ts',
+        dirs: ['src/composables'],
+        vueTemplate: true,
+      }),
+      Components({
+        resolvers: [
+          PrimeVueResolver({
+            importStyle: false,
+            importIcons: true,
+            importTheme: false,
+          }),
+        ],
+        dirs: [
+          'src/components',
+          'src/domain/**/components',
+        ],
+        dts: 'src/components.d.ts',
+      }),
+      // insert our plugin last so it runs before other plugins for `mocks` replacement
+      excludeMocksPlugin,
+    ],
+    resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
-})
+    },
+  };
+});
